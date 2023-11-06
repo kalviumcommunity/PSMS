e4d59fdfcf9b176d3f105995e41794b2694464f4
SomendraZ <113346559+SomendraZ@users.noreply.github.com>
2023-11-07 00:04:38 +0530

    Milestone 4 :- DBMS : DML commands.

---
+++ db/cleanUp.sql
@@ -0,0 +1,11 @@
+-- Drop the PSMS database if it exists
+DROP DATABASE IF EXISTS PSMS;
+
+-- Drop the tables if they exist
+DROP TABLE IF EXISTS AuditLog;
+DROP TABLE IF EXISTS UserAccounts;
+DROP TABLE IF EXISTS Reports_Case;
+DROP TABLE IF EXISTS Police_Personal;
+DROP TABLE IF EXISTS PoliceStation;
+DROP TABLE IF EXISTS ReportCategory;
+DROP TABLE IF EXISTS Role;
